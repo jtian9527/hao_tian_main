e166d6da9395cc1da672dad6fd170ee11c6b0c4e
hao.tian <hao.tian@shopee.com>
2022-09-18 00:10:38 +0800

    upadte

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ps -aux | grep python | grep 8010 | grep -v grep  | awk '{print $2}' | xargs kill -9
+echo "stop 8010"
 sleep 2
+echo "start 8010"
 nohup python manage.py runserver 0.0.0.0:8010 > server.log 2>&1 &
+echo "finish 8010"
